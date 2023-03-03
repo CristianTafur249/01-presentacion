3d8d04a9c0c744ce1a3dd897dfb543419f27080d
CristianTafur249 <cristian2191970@correo.uis.edu.co>
2023-03-03 16:09:08 -0500

    Primer commt temporizador

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="App">
-      <PresentationCard /><Contador />
+      <PresentationCard /><Contador /> <CountDown/>
     </div>
   )
 }
---
+++ src/Tempr.jsx
@@ -0,0 +1,8 @@
+function CountDown(){
+    return{
+        <>
+        <p>¿Cuantos segundos desea contar?</p>
+        </>
+    }
+}
+export default CountDown;
