7d7dc9b31b0804c908148a20220b0e8ae7e0c71e
CristianTafur249 <cristian2191970@correo.uis.edu.co>
2023-03-03 13:40:25 -0500

    presentation card cambios

---
+++ src/PresentationCard.jsx
@@ -0,0 +1,17 @@
+let name ='Cristian';
+let lastname='Tafur';
+import imgPerfil from './assets/avatar.jpg';
+
+function PresentationCard(){
+    return(
+        <div className="presentation-card">
+            <h1>
+                Hola, soy { name + " " + lastname }
+            </h1>
+            <img src={imgPerfil} alt="Avatar" className='Avata-img'/>
+        </div>
+        
+    );
+
+}
+export default PresentationCard
